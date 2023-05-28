215aad5b3730f253102e28539ce006ee175e0d4a
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-05-28 20:09:07 +0100

    chore: update service provider

--- src/MapleradServiceProvider.php
+++ src/MapleradServiceProvider.php
@@ -6,4 +6,44 @@ use Illuminate\Support\ServiceProvider;
 
 class MapleradServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->registerPublishing();
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->configure();
+    }
+
+    /**
+     * Setup the configuration for Maplerad.
+     */
+    protected function configure(): void
+    {
+        $this->mergeConfigFrom(
+            path: __DIR__.'/../config/maplerad.php',
+            key: 'maplerad'
+        );
+    }
+
+    /**
+     * Register the package's publishable resources.
+     */
+    protected function registerPublishing(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/maplerad.php' => $this->app->configPath('maplerad.php'),
+            ], 'maplerad-config');
+        }
+    }
 }
