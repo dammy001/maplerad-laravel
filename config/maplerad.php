11e91f82931848b4a4666595accf36b92c316764
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-05-28 20:01:59 +0100

    chore: add config

---
+++ config/maplerad.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    /*
+        |--------------------------------------------------------------------------
+        | Maplerad API Key
+        |--------------------------------------------------------------------------
+        |
+        | The Maplerad API key give you access to Maplerad's API. The "secret_key" is
+        | typically used to make a request to the API.
+        |
+        */
+
+    'secret_key' => env('RESEND_SECRET_KEY'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Domain
+    |--------------------------------------------------------------------------
+    |
+    | This is the domain where the apis will be accessible from.
+    |
+    */
+
+    'domain' => env('MAPLERAD_DOMAIN', 'https://api.maplerad.com/v1/'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Maplerad Webhooks
+    |--------------------------------------------------------------------------
+    |
+    | Your Maplerad webhook secret is used to prevent unauthorized requests to
+    | your Maplerad webhook handling controllers. The tolerance setting will
+    | check the drift between the current time and the signed request's.
+    |
+    */
+
+    'webhook' => [
+        'secret' => env('MAPLERAD_WEBHOOK_SECRET'),
+        'tolerance' => env('MAPLERAD_WEBHOOK_TOLERANCE', 300),
+        'events' => [],
+    ],
+];
