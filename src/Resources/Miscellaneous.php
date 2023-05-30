a25e46b3c6618ba4eadb1296afaf38d54962fd11
Damilare Anjorin <damilareanjorin1@gmail.com>
2023-05-30 13:17:00 +0100

    chore: update miscellenous

--- src/Resources/Miscellaneous.php
+++ src/Resources/Miscellaneous.php
@@ -16,6 +16,8 @@ final class Miscellaneous
     /**
      * Get list of available currencies
      *
+     * @see https://maplerad.dev/reference/get-all-currencies
+     *
      * @throws RequestException
      */
     public function currencies(): CurrenciesResponse
@@ -25,5 +27,40 @@ final class Miscellaneous
         $result = $this->transporter->get($payload->uri)->throw();
 
         return CurrenciesResponse::from(attributes: (array) $result->json('data'));
+    }
+
+    /**
+     * Get list of available countries
+     *
+     * @see https://maplerad.dev/reference/get-all-countries
+     *
+     * @throws RequestException
+     */
+    public function countries(): array
+    {
+        $payload = Payload::list('countries');
+
+        $result = $this->transporter->get($payload->uri)->throw();
+
+        return (array) $result->json('data');
+    }
+
+    /**
+     * Verify bank account
+     *
+     * @see https://maplerad.dev/reference/resolve-institution-account
+     *
+     * @throws RequestException
+     */
+    public function resolveAccount(string|int $accountNumber, string|int $bankCode): array
+    {
+        $payload = Payload::create(
+            'institutions/resolve',
+            ['account_number' => (string) $accountNumber, 'bank_code' => (string) $bankCode]
+        );
+
+        $result = $this->transporter->post($payload->uri, $payload->parameters)->throw();
+
+        return (array) $result->json('data');
     }
 }
